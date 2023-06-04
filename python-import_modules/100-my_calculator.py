aa354e5d50316949b072bfb2a2b3a7cc71439203
alejandr088 <alejandrivello@gmail.com>
2023-06-04 15:09:44 -0700

    tweaks on task 100

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
+
     from calculator_1 import add, sub, mul, div
 
     def calculator():
@@ -22,6 +23,7 @@ if __name__ == '__main__':
             result = div(a, b)
         else:
             print('Unknown operator. Available operators: +, -, * and /')
+
             return 1
         print('{} {} {} = {}'.format(a, operator, b, result))
 
