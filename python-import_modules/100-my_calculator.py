37550c2747ce9fc03e7e1c8cd1a9670e7b6905d1
alejandr088 <alejandrivello@gmail.com>
2023-06-04 13:38:23 -0700

    tasks 100 - 103

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    def calculator():
+        if len(sys.argv) != 4:
+            print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+            return 1
+
+        a = int(sys.argv[1])
+        operator = sys.argv[2]
+        b = int(sys.argv[3])
+
+        if operator == '+':
+            result = add(a, b)
+        elif operator == '-':
+            result = sub(a, b)
+        elif operator == '*':
+            result = mul(a, b)
+        elif operator == '/':
+            result = div(a, b)
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            return 1
+        print('{} {} {} = {}'.format(a, operator, b, result))
+
+calculator()
---
+++ python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+import string
+print(''.join([chr(65 + i) for i in range(26)]))
