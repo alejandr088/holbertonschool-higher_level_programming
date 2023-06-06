b4624a933e85fd878e38ff4d20edc64b6f1c83e7
alejandr088 <alejandrivello@gmail.com>
2023-06-06 13:18:30 -0700

    fixed tasks 0, 2, 3, 7, 8

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,12 +2,11 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if (len(tuple_a) < 2):
         tuple_a += (0, 0)
-    elif (len(tuple_b) < 2):
+    if (len(tuple_b) < 2):
         tuple_b += (0, 0)
-    el_1a = tuple_a[0]
-    el_1b = tuple_b[0]
-    eladd = el_1a + el_1b
 
-    tuple_f = (eladd, tuple_a[1] + tuple_b[1])
+    eladd = tuple_a[0] + tuple_b[0]
+    elsumm = tuple_a[1] + tuple_b[1]
+    tuple_f = (eladd, elsumm)
 
     return tuple_f
