c956719560f3e1301a82715fec9d4c991805ed1b
root <you@example.com>
2023-06-09 11:59:09 -0700

    fixed pycodestyle

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-        if a_dictionary is None:
-                return None
-        high = max(a_dictionary, key=a_dictionary.get)
-        return high
+    if a_dictionary is None:
+        return None
+    high = max(a_dictionary, key=a_dictionary.get)
+    return high
