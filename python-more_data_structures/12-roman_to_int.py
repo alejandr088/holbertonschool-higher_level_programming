02fb0d994fabd3b9f3df5ccc0cd6c04213bc33bc
root <you@example.com>
2023-06-11 14:47:13 -0700

    tasks 0 to 6 & 12 prev

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-
-
-
-
-    if roman_string is None or roman_string != str:
-        return 0
+    if isinstance(roman_string, str) and roman_string is not None:
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+        prev_value = 0
+        result = 0
+        for symbol in roman_string:
+            value = roman_values.get(symbol, 0)
+            result += value if value <= prev_value else value - 2 * prev_value
+            prev_value = value
+        return result
+    return 0
