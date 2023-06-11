3e5b40783c17b51777fe32e262e8ad25e48492d5
root <you@example.com>
2023-06-11 15:37:16 -0700

    rollback to older res

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if isinstance(value, int):
-            print('{:d}'.format(value))
-            return True
+        value = int(value)
+        print('{:d}'.format(value))
+        return True
     except ValueError:
         return False
     except TypeError:
-        return True
+        return False
