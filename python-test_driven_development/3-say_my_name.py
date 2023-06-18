08faf7b787dd748d3eb7c989d2db2591408d5223
alejandr088 <alejandrivello@gmail.com>
2023-06-18 15:29:42 -0700

    tasks 0 to 6 fixed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,5 +5,14 @@ function that print a name
 
 def say_my_name(first_name, last_name=""):
     """
-    
+    function that prints first_name and last_name
     """
+
+    if not isinstance(first_name, str):
+            raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+            raise TypeError('last_name must be a string')
+
+    if isinstance(first_name, str) and \
+    isinstance(last_name, str):
+        print(f'My name is {first_name} {last_name}')
--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    text = text.replace('.', '.\n\n').replace \
+        ('?', '?\n\n').replace(':', ':\n\n')
+    lines = text.split('\n')
+    result = '\n'.join(line.strip() for line in lines)
+    print(result, end="")
