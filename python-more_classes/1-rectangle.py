8e2b0085a16b4fd45f9dbcaaf6f76ba2eadfbb5a
root <you@example.com>
2023-06-19 12:18:57 -0700

    fixed folder name

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+a function that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    class that repr a rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        initializate rectangle
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """
+        returns the width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        returns the height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        set the width
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """
+        set the height
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
