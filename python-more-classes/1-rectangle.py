16ee2b88ee324fc2b85f3ef5f927bdbd14a94d6f
root <you@example.com>
2023-06-19 12:13:19 -0700

    tasks 2 to 9

--- python-more-classes/1-rectangle.py
+++ python-more-classes/1-rectangle.py
@@ -3,6 +3,7 @@
 a function that defines a rectangle
 """
 
+
 class Rectangle:
     """
     class that repr a rectangle
@@ -12,8 +13,8 @@ class Rectangle:
         """
         initializate rectangle
         """
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -22,14 +23,6 @@ class Rectangle:
         """
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-    
     @property
     def height(self):
         """
@@ -37,13 +30,24 @@ class Rectangle:
         """
         return self.__height
 
+    @width.setter
+    def width(self, value):
+        """
+        set the width
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
     @height.setter
     def height(self, value):
         """
         set the height
         """
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError('height must be >= 0')
         self.__height = value
--- python-more-classes/6-rectangle.py
+++ python-more-classes/6-rectangle.py
@@ -1,1 +1,78 @@
 #!/usr/bin/python3
+"""
+a function that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    class that repr a rectangle
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """
+        initializate rectangle
+        """
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """
+        returns the width
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        returns the height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        set the width
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """
+        set the height
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return self.__height * 2 + self.__width * 2
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ''
+        rectangle_str = ''
+        for _ in range(self.__height):
+            rectangle_str += '#' * self.__width + '\n'
+        return rectangle_str[:-1]
+
+    def __repr__(self):
+        return f'Rectangle({self.__width}, {self.__height})'
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print('Bye rectangle...')
