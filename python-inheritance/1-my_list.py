421bd01f1779238edf658552646d65de88378b7a
root <you@example.com>
2023-06-24 23:53:57 -0700

    fixed code

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,6 +6,5 @@ class MyList(list):
     """class that inherits list """
     def print_sorted(self):
         """function that prints itself but sorted"""
-        copya = self.copy()
-        copya.sort()
+        copya = sorted(self)
         print(copya)
