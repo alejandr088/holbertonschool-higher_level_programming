df9d913a0f62dadc2c5cf570a6a9d38537be4ade
root <you@example.com>
2023-06-26 12:45:12 -0700

    fixed tasks 7, 10 & 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""author: alejandr088"""
+
+
+class Student:
+    """a class that represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """a funct that init class student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """a funct that ret a dict representation"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json):
+        """a funct that replaces attribs"""
+        for key, value in json.items():
+            setattr(self, key, value)
