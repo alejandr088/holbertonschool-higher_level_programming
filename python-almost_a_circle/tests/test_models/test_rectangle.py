67028f7af724fdb068de214c7d2f686e98b2adfa
Alejandro <alejandrivello@gmail.com>
2023-07-04 10:45:04 -0700

    fixed cache

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,14 +7,17 @@ from models.rectangle import Rectangle
 class TestRectangle(unittest.TestCase):
     """test rectangle class"""
     def test_area(self):
+        """test area"""
         rectangle = Rectangle(3, 4)
         self.assertEqual(rectangle.area(), 12)
 
     def test_str(self):
+        """test string"""
         rectangle = Rectangle(2, 3, 4, 5, 1)
         self.assertEqual(str(rectangle), '[Rectangle] (1) 4/5 - 2/3')
 
     def test_update_args(self):
+        """test updt args"""
         rectangle = Rectangle(1, 2, 3, 4, 5)
         rectangle.update(6, 7, 8, 9, 10)
         self.assertEqual(rectangle.id, 6)
@@ -24,6 +27,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rectangle.y, 10)
 
     def test_update_kwargs(self):
+        """test updt kwargs"""
         rectangle = Rectangle(1, 2, 3, 4, 5)
         rectangle.update(width=6, height=7, x=8, y=9, id=10)
         self.assertEqual(rectangle.id, 10)
@@ -33,6 +37,7 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(rectangle.y, 9)
 
     def test_to_dictionary(self):
+        """test dict"""
         rectangle = Rectangle(2, 3, 4, 5, 1)
         expected_dict = {'id': 1, 'width': 2, 'height': 3, 'x': 4, 'y': 5}
         self.assertEqual(rectangle.to_dictionary(), expected_dict)
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -7,10 +7,12 @@ from models.square import Square
 class TestSquare(unittest.TestCase):
     """test square class"""
     def test_size(self):
+        """test size"""
         square = Square(2)
         self.assertEqual(square.size, 2)
 
     def test_size_setter(self):
+        """test size sett"""
         square = Square(2)
         square.size = 3
         self.assertEqual(square.size, 3)
@@ -18,10 +20,12 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.height, 3)
 
     def test_str(self):
+        """test str"""
         square = Square(2, 3, 4, 1)
         self.assertEqual(str(square), '[Square] (1) 3/4 - 2')
 
     def test_update_args(self):
+        """test updt args"""
         square = Square(1, 2, 3, 4)
         square.update(5, 6, 7, 8)
         self.assertEqual(square.id, 5)
@@ -30,6 +34,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.y, 8)
 
     def test_update_kwargs(self):
+        """test updt kwargs"""
         square = Square(1, 2, 3, 4)
         square.update(size=5, x=6, y=7, id=8)
         self.assertEqual(square.id, 8)
@@ -38,6 +43,7 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.y, 7)
 
     def test_to_dictionary(self):
+        """test dict"""
         square = Square(2, 3, 4, 1)
         expected_dict = {'id': 1, 'size': 2, 'x': 3, 'y': 4}
         self.assertEqual(square.to_dictionary(), expected_dict)
